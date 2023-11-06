04f0980ef03c04b083028136618f88cc8fdf7c98
DoseCalculator <150090951+DoseCalculator@users.noreply.github.com>
2023-11-07 12:14:19 +1300

    initial commit

---
+++ script.js
@@ -0,0 +1,93 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 150;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+
+ScrollReveal().reveal(
+  ".home-img, .download-container, .screenshots-box, .contact form",
+  { origin: "bottom" }
+);
+
+toastr.options = {
+  positionClass: "toast-top-right",
+  preventDuplicates: true,
+  showDuration: "300",
+  hideDuration: "1000",
+  timeOut: "5000",
+  extendedTimeOut: "1000",
+  closeButton: "true",
+};
+
+toastr.options.showMethod = "slideDown";
+toastr.options.hideMethod = "slideUp";
+toastr.options.closeMethod = "slideUp";
+
+(function () {
+  emailjs.init("GszwseblWsql_Xa53");
+})();
+
+function sendMail() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  if (!name || !email || !message) {
+    toastr.error("Please fill out all fields.", "Error");
+    return;
+  }
+
+  var params = {
+    from_name: name,
+    from_email: email,
+    message: message,
+  };
+
+  emailjs.send("service_g7k0b2p", "template_8akkimr", params).then(
+    function (response) {
+      toastr.success("Email sent successfully!", "Success");
+      document.getElementById("contact-form").reset();
+    },
+    function (error) {
+      toastr.error("Failed to send email. Please try again later.", "Error");
+    }
+  );
+}
